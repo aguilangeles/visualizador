be06b825fc2672769a820a125d58011fe48755f9
Maria de los Angeles <aguilangeles@gmail.com>
2014-08-04 16:59:17 -0300

    ordena satisfactoriamente

--- js/getImageInfo.js
+++ js/getImageInfo.js
@@ -1,59 +1,62 @@
 function getImageInfo(items, id)
 {
-    var infoId = items;
-    id = infoId;
-    var query = $("#query_" + id).html();
+      var infoId = items;
+      id = infoId;
+      var query = $("#query_" + id).html();
 
-    query = encodeURIComponent(query);
+      query = encodeURIComponent(query);
 
-    items = $("#imageData" + items).html();
-    if (!$("#row" + id).hasClass('fetched'))
-    {
-        $("#downloading").dialog({title: "Cargando Datos"})
-        $("#downloading").dialog("open");
-        $("#row" + id).addClass('fetched');
-	
-        $.ajax({url: "/images/getImagesById",
-            context: document.body,
-            type: "POST",
-            data: "items=" + items + "&infoId=" + infoId + "&query=" + query,
-            dataType: "json",
-            success: function(data) {
-                $("#row" + id).prepend(data.html);
-                $("#imageData" + id).empty();
-                $("#imageData" + id).html(data.imageData);
-                $("#downloading").dialog("close");
-	        $("#"+id).tablesorter({
-		      sortList: [[1,0]]
-		      
-		}).trigger('update');	
-	    }
-        });
-         }
-    $("#row" + id).toggle();
+      items = $("#imageData" + items).html();
+      if (!$("#row" + id).hasClass('fetched'))
+      {
+	    $("#downloading").dialog({title: "Cargando Datos"})
+	    $("#downloading").dialog("open");
+	    $("#row" + id).addClass('fetched');
+
+	    $.ajax({url: "/images/getImagesById",
+		  context: document.body,
+		  type: "POST",
+		  data: "items=" + items + "&infoId=" + infoId + "&query=" + query,
+		  dataType: "json",
+		  success: function(data) {
+			$("#row" + id).prepend(data.html);
+			$("#imageData" + id).empty();
+			$("#imageData" + id).html(data.imageData);
+			$("#downloading").dialog("close");
+			$("#" + id).tablesorter({
+			      sortReset: true,
+			      sortRestart: true,
+			      sortInitialOrder: 'asc'
+
+			});
+		  }
+	    });
+      }
+      $("#row" + id).toggle();
 
 }
 function seeMore(id)
-    {
-        $("#downloading").dialog({title: "Cargando Datos"});
-        $("#downloading").dialog("open");
-        var query = $("#query_" + id).html();
-        var imageList = $("#imageList" + id).html();
-        $.ajax({url: "/more/seeMore",
-            context: document.body,
-            type: "POST",
-            data: "query=" + query + "&imageList=" + imageList,
-            dataType: "json",
-            success: function(data) {
-                $("#" + data.id).append(data.table);
-                $("#imageList" + id).empty();
-                $("#imageList" + id).html(data.imageList);
-                if (!data.hasMore) {
-                    $("#seeMore" + data.id).remove();
-                }
-                $("#downloading").dialog("close");
+{
+      $("#downloading").dialog({title: "Cargando Datos"});
+      $("#downloading").dialog("open");
+      var query = $("#query_" + id).html();
+      var imageList = $("#imageList" + id).html();
+      $.ajax({url: "/more/seeMore",
+	    context: document.body,
+	    type: "POST",
+	    data: "query=" + query + "&imageList=" + imageList,
+	    dataType: "json",
+	    success: function(data) {
+		  $("#" + data.id).append(data.table);
+		  $("#imageList" + id).empty();
+		  $("#imageList" + id).html(data.imageList);
+		  if (!data.hasMore) {
+			$("#seeMore" + data.id).remove();
+		  }
+		  $("#downloading").dialog("close");
 		$("#"+id).tablesorter().trigger('update'); 	
-            }
-        });
-    }
+		$("#"+id).tablesorter().trigger('"appendCache"'); 	
+	    }
+      });
+}
 
